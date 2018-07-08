2729e3781705314b63888fa2106b456f875f2800
zhouxiaohui <evanxiaohui@163.com>
2018-07-08 13:27:54 +0800

    Initial commit

---
+++ app/src/main/java/com/evan/evanfloat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.evan.evanfloat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
